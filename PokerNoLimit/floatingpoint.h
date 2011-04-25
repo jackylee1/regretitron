b04f10ff7ccfaa2bebd7d672212262a90c141261
Scott Kathrein <scottopoly@gmail.com>
2011-04-25 06:34:54 +0000

    Changes from last summer: enhancements to my floating point class to make it more better. (I'm not sure)
    git-svn-id: file:///home/scott/regretitron_publish/regretitron_filtered3_svn/trunk@143 56bb13ec-af9c-4a7e-874f-62681e7d7c12

--- PokerNoLimit/floatingpoint.h
+++ PokerNoLimit/floatingpoint.h
@@ -4,14 +4,12 @@
 #include <ieee754.h>
 #include <boost/static_assert.hpp>
 #include <boost/math/special_functions/pow.hpp> //http://www.boost.org/doc/libs/1_41_0/libs/math/doc/sf_and_dist/html/math_toolkit/special/powers/ct_pow.html
-#ifndef DONT_INCLUDE_UTILITY
 #include "../utility.h"
-#endif
 
 //adapted to use uint64 instead of 2 uint32's
 union my_ieee754_double
 {
-	double d;
+	double f; //f to match the float one to use the included file polymorphically
 
 	/* This is the IEEE 754 double-precision format.  */
 	struct
@@ -30,20 +28,35 @@ union my_ieee754_double
 	} ieee;
 };
 
-const int SUPER_MANTBITS = 52; //number of bits in the mantissa of the "SUPER" type, here double.
+const int SUPER_MANTBITS_DOUBLE = 52; //number of bits in the mantissa of the "SUPER" type, here double.
+const int SUPER_MANTBITS_FLOAT = 23; //number of bits in the mantissa of the "SUPER" type, here float.
 
 // I use the same header to generate the two classes. No better way to remove that bit from the representation.
 // We're going for the bits!
 
-#define SIGNED 12345
-#define UNSIGNED 67890
+#define SIGNED 12
+#define UNSIGNED 34
+#define FLOAT 56
+#define DOUBLE 78
+#define MASTERTYPE FLOAT
 #define SIGNEDNESS SIGNED
 #include "floatingpoint_internal.h"
 #undef SIGNEDNESS
 #define SIGNEDNESS UNSIGNED
 #include "floatingpoint_internal.h"
 #undef SIGNEDNESS
+#undef MASTERTYPE
+#define MASTERTYPE DOUBLE
+#define SIGNEDNESS SIGNED
+#include "floatingpoint_internal.h"
+#undef SIGNEDNESS
+#define SIGNEDNESS UNSIGNED
+#include "floatingpoint_internal.h"
+#undef SIGNEDNESS
+#undef MASTERTYPE
 #undef SIGNED
 #undef UNSIGNED
+#undef FLOAT
+#undef DOUBLE
 
 #endif
