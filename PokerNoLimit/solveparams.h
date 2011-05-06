544fe10f99148cea2d68b56e3356896d492839c8
Scott Kathrein <scottopoly@gmail.com>
2011-05-06 04:08:25 +0000

    Adding some comments about some tests I ran and the current situation
    git-svn-id: file:///home/scott/regretitron_publish/regretitron_filtered3_svn/trunk@155 56bb13ec-af9c-4a7e-874f-62681e7d7c12

--- PokerNoLimit/solveparams.h
+++ PokerNoLimit/solveparams.h
@@ -11,6 +11,16 @@
 
 //solver settings
 
+/* DIGRESSION ON MY FLOATING POINT NUMBERS
+   I have implemented tiny (1-byte) template-parameter-cofigurable floating point numbers that work
+   in exactly the same way as IEEE floating point numbers -- mine just use fewer bits -- and mine
+   also use randomness to choose which representable number to use. There may be a fruitful avenue 
+   to improve them further (see future developments below), but for now, these work.
+
+   The effects of all these parameters can be seen by simply printing out the 256 representable
+   numbers by type-punning to a uint8.
+   ***/
+
 /* MAXEXPNEEDED:
    the third template parameter in a FloatCustom is the MAXEXPNEEDED. This number defines the maximum 
    exponent representable by the floating point number representation. It is a way of specifying
@@ -28,37 +38,119 @@
    maximal representable number is just under: 
    			2^( 1 + MAXEXPNEEDED )
 
-   EXPBITS then defines the amount of exponent space that is used for negative exponents. For example, 
-   if EXPBITS is 3, and MAXEXPNEEDED is 4, then raw exponent value 1 to 7 would represent actual
-   exponent values -2 to 4. A raw exponent value of 0 represents a denormalized number.
+   EXPBITS then defines the amount of exponent space that is used for exponents less than MAXEXPNEEDED. 
 
-   for full stacksizes and a bigblind value of 2, a MAXEXPNEEDED of 16 is sufficient for regrets on 
-   the river to keep them from overflowing. (5, 6, 8, 10, 10 bins)
+   Since the bigblind value defines the scale of the solution, using larger numbers requires a larger
+   value of MAXEXPNEEDED. Here are needed values of MAXEXPNEEDED for a variety of 4-bin solutions.
 
-   for full stacksizes and a bigblind value of 10, a MAXEXPNEEDED of 16 is insufficient for regrets on 
-   the river to keep them from overflowing. (5 bins) (failed around 60M iterations)
+   At 4 bins, full stacksize, and bigblind value of 10, result of MAXEXPNEEDED values 
+   used for 16-bit STRATN's:
+     32 through 17 => solves completely (45M iterations)
+     16 => overflows between 32M and 36M iterations
+     15 => overflows between 16M and 20M iterations
+     14 => overflows between 8M and 12M iterations
+     13 => overflows between 4M and 8M iterations
+     12 => overflows before 4M iterations
+     11 => overflows before 4M iterations
+     10 => overflows before 4M iterations
 
-   for full stacksizes and a bigblind value of 10, a MAXEXPNEEDED of 18 is sufficient for regrets on
-   the river to keep them from overflowing. (5 bins)
-   */
+   At 4 bins, full stacksize, and bigblind value of 10, result of MAXEXPNEEDED values 
+   used for 8-bit REGRET's:
+     18 => solves completely (45M iterations)
+     17 => solves completely
+     16 => overflows between 16M and 20M iterations
+     15 => overflows between 12M and 16M iterations
 
+   ***/
 
-//this one is good for river stratn
-typedef FloatCustomUnsigned< uint16, 6, 32, true > FloatCustomUnsigned_uint16_6_32_true;
-//these are good for river regret
-typedef FloatCustomSigned< uint8, 5, 18, true > FloatCustomSigned_uint8_5_18_true;
-typedef FloatCustomSigned< uint16, 5, 18, true > FloatCustomSigned_uint16_5_18_true;
+/* EXPBITS
+
+   As stated above EXPBITS defines the amount of exponent space used for values less than MAXEXPNEEDED.
+   The effects of adding or removing a bit is dramatic, many expbits quickly allow for extremely tiny 
+   representable numbers, few expbits make the smallest positive representable number around 10^3.
+   The only correct way to decide what's best is by testing. 
+
+   The following is all 5-bin games solved to 100M iterations with MAXEXPNEEDED at 18, and bblind at 10
+   All tests use doubles on the preflop, flop, and turn for stratn, and floats on the preflop, flop, and turn
+   for regrets, with float working type.
+
+   results of playoffs vs ref-5-M100.xml (at 320M games .. significant to +- 0.251558 milli-big-blinds / hand)
+
+   with river regret as an 8-bit CustomFloat with EXPBITS = N, and river stratn as a float
+   N = 6: -2.319
+   N = 5: -1.087
+   N = 4: -0.346
+   N = 3: -5.831
+   N = 2: -31.38
+   N = 1: -51.06
+
+   with river stratn as an 8-bit CustomFloat with EXPBITS = N, and river regret as a float
+   N = 7: overflowed 	(with MAXEXPNEEDED = 18)
+   N = 6: overflowed 	(with MAXEXPNEEDED = 18)
+   N = 7: overflowed	(with MAXEXPNEEDED = 19)
+   N = 6: -0.078		(with MAXEXPNEEDED = 19)
+   N = 5: +0.057
+   N = 4: +0.065
+   N = 3: -0.186
+   N = 2: -1.851
+   N = 1: -3.421
+
+   From these data, it seems using 4-bits is best. So I did a further test
+
+   with river stratn AND river regret as 8-bit CustomFloat with EXPBITS = 4...
+   ==> wins -0.466 against ref-5-M100.xml after 2.607 billion games (+- 0.088 mbb/hand)
+
+   As a control, I tried with river stratn AND river regret as 4-byte floats...
+   ==> wins +0.039 against ref-5-M100.xml after 2.620 billion games (+- 0.088 mbb/hand)
+
+   ***/
+
+/* PERFORMANCE
+
+   The main drawback of the FloatCustom is performance, when solving the above two strategies
+   one using EXPBITS = 4 for both river regret and river stratn, and the other using all floats,
+   I got these iterations per second on average for the *entire* 100 million iterations.
+
+   both4: 7759  
+   float: 11080
+
+   The one-byte floats use 4 times less memory, and allow me to solve about a 4 times larger
+   strategy, which at the same speed would take 4 times longer, but instead takes 5.71 times longer.
+
+   ***/
+
+/* FUTURE DEVELOPMENTS
+
+   Seeing how single byte floats work well, it would be totally reasonable to use a lookup table in place 
+   of all this floating point bull crap. That way I could choose the representable numbers at will, and ensure
+   they are exactly exponentially distributed. Reading a float would be far faster than the current method,
+   and with work, I could optimize the performance of writing a float. Writing a float involves finding the 
+   two representable numbers in the table closest to the desired number. For stratn's it makes sense to optimize 
+   for adding a small number to the existing one in the += operator. For regret's the best strategy is less clear,
+   a tree search would be an obvious contender.
+
+   ***/
+
+#define CUSTOMFLOATNAME_( sign, temp1, temp2, temp3, temp4 ) FloatCustom##sign##_##temp1##_##temp2##_##temp3##_##temp4
+#define CUSTOMFLOATNAME( sign, temp1, temp2, temp3, temp4 ) CUSTOMFLOATNAME_( sign, temp1, temp2, temp3, temp4 )
+
+//unsigned are good for river stratn
+typedef FloatCustomUnsigned< uint8, 4, 18, true > CUSTOMFLOATNAME( Unsigned, uint8, 4, 18, true );
+
+//signed are good for river regret
+typedef FloatCustomSigned< uint8, 4, 18, true > CUSTOMFLOATNAME( Signed, uint8, 4, 18, true );
+
 //stratn's are write only
 #define SOLVER_TYPES (9, ( \
 ( Working_type, float ), \
 ( PFlopStratn_type, double ), \
 ( FlopStratn_type, double ), \
 ( TurnStratn_type, double ), \
-( RiverStratn_type, FloatCustomUnsigned_uint16_6_32_true ), \
+( RiverStratn_type, float ), \
 ( PFlopRegret_type, float ), \
 ( FlopRegret_type, float ), \
 ( TurnRegret_type, float ), \
-( RiverRegret_type, FloatCustomSigned_uint8_5_18_true )))
+( RiverRegret_type, float )))
 
 #define SEPARATE_STRATN_REGRET 1
 #define DYNAMIC_ALLOC_STRATN 0 /*must be false for __float128 to work due to alignment issues*/
